b86d534aac98dc726d23575b876ee19441584d93
Pawel Krzyzak <Pawel.Krzyzak@krolldiscovery.com>
2024-06-07 19:32:36 +0200

    update

--- BettingTournament/Core/Models/Round.cs
+++
@@ -1,9 +0,0 @@
-﻿using BettingTournament.Core.Models;
-
-namespace BettingTournament.Core
-{
-    public class Round
-    {
-        public IEnumerable<ActiveGame> Games { get; set; } = [];
-    }
-}
